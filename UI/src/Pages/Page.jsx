208d188970413f85034dc07c338cc68bd6ba628f
Kumait <Kumait@gmail.com>
2024-04-01 16:46:06 +0200

    Local

--- UI/src/Pages/Page.jsx
+++ UI/src/Pages/Page.jsx
@@ -1,16 +1,15 @@
 import Register from "../Components/Registers/Register.jsx";
-import img from '../assets/img.svg';
+import img from "../assets/img.svg";
 import Login from "../Components/Registers/Login.jsx";
 const Page = () => {
-    return (
-        <>
-            <Login/>
-            <img src={img} alt="img"/>
-            <Register/>
-            <h1>Page</h1>
-
-        </>
-    );
+  return (
+    <>
+      <Login />
+      <img src={img} alt="img" />
+      <Register />
+      <h1>Page</h1>
+    </>
+  );
 };
 
 export default Page;
