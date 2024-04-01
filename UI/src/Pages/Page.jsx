4e8eb74a5ed0f8c1efc59a3dba580a28febdb471
Kumait <Kumait@gmail.com>
2024-04-01 19:57:22 +0200

    Submitting

--- UI/src/Pages/Page.jsx
+++ UI/src/Pages/Page.jsx
@@ -1,13 +1,20 @@
+import stylex from "@stylexjs/stylex";
+import styles from "../styles.jsx";
 import Register from "../Components/Registers/Register.jsx";
 import img from "../assets/img.svg";
+import key from "../../public/key.svg";
 import Login from "../Components/Registers/Login.jsx";
+import Submitting from "../Components/Submit/Submitting.jsx";
+
 const Page = () => {
   return (
     <>
       <Login />
+      <img src={key} alt="logo" {...stylex.props(styles.img)} />
       <img src={img} alt="img" />
       <Register />
-      <h1>Page</h1>
+      <h3>Submitting</h3>
+      <Submitting />
     </>
   );
 };
